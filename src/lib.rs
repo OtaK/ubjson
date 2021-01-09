8b11c5cb9d19d0964ef4c1208acfad750de23ff0
Mathieu Amiot <m.amiot@otak-arts.com>
2021-01-09 17:41:41 +0100

    Added marker enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Marker {
+    Null = b'Z',
+    Noop = b'N',
+    True = b'T',
+    False = b'F',
+    Int8 = b'i',
+    Uint8 = b'U',
+    Int16 = b'I',
+    Int32 = b'l',
+    Int64 = b'L',
+    Float32 = b'd',
+    Float64 = b'D',
+    HighPrecisionNumber = b'H',
+    Char = b'C',
+    String = b'S',
+    ArrayStart = b'[',
+    ArrayEnd = b']',
+    ObjectStart = b'{',
+    ObjectEnd = b'}',
 }
